7a0261675e08976a3719090f45d4e3d32f24a409
Artem Belozertsev <Artembelozertsev2006@gmail.com>
2026-05-26 18:00:06 +0100

    Stack Process cards on mobile (1-col), 2-col tablet, 4-col desktop

--- components/Process.tsx
+++ components/Process.tsx
@@ -20,7 +20,7 @@ export default function Process() {
           real results.
         </h2>
 
-        <div className="grid grid-cols-2 gap-4 sm:grid-cols-4 lg:gap-6">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4 lg:gap-6">
           {steps.map((s) => (
             <div
               key={s.num}
